31395c23c3ad4d1fffe1025e35395edd906421d5
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2022-06-11 10:03:11 +0300

    Functions

---
+++ Programming Fundamentals - may 2022/Functions/Repeat String/repeat_string.py
@@ -0,0 +1,6 @@
+def repeater(str, count):
+    return str * count
+
+current_string = input()
+current_count = int(input())
+print(repeater(current_string, current_count))
---
+++ Programming Fundamentals - may 2022/Functions/Rounding/rounding.py
@@ -0,0 +1,8 @@
+base_list = input().split(" ")
+final_list = list()
+
+for n in base_list:
+    final_n = round(float(n))
+    final_list.append(final_n)
+
+print(final_list)
