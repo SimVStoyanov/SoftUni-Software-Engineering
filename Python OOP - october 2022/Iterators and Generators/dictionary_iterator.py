d00db50448d560f98b4375bf914650d9461981fc
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2022-11-22 22:13:03 +0200

    Iterators and Generators

---
+++ Python OOP - october 2022/Iterators and Generators/dictionary_iterator.py
@@ -0,0 +1,21 @@
+class dictionary_iter:
+    def __init__(self, dictionary: dict):
+        self.items = list(dictionary.items())
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.items):
+            raise StopIteration
+
+        result = self.items[self.index]
+        self.index += 1
+
+        return result
+
+
+result = dictionary_iter({"name": "Peter", "age": 24})
+for x in result:
+    print(x)
