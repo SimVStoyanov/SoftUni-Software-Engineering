49a8bb744322785ae4f383704c61270ddf1507c3
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-06 11:26:26 +0300

    Key Concepts

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/02.KeyConcepts/04.Tiles/Program.cs
@@ -0,0 +1,14 @@
+﻿double bathroomWidth = double.Parse(Console.ReadLine());
+double bathroomHeight = double.Parse(Console.ReadLine());
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeight = double.Parse(Console.ReadLine());
+
+double bathroomArea = bathroomWidth * bathroomHeight;
+double finalBathroom = bathroomArea + (bathroomArea * 0.1);
+double tileArea = tileWidth * tileHeight;
+double tilesNeeded = finalBathroom / tileArea;
+
+Console.WriteLine(Math.Round(tilesNeeded));
+
+
+ 
