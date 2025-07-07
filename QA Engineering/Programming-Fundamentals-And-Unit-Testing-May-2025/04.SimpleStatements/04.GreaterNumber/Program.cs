535078ae4216b6276414d48e349d48d06b9425ee
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-07 23:31:34 +0300

    Simple Statements

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/04.SimpleStatements/04.GreaterNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1 >= n2)
+{
+    Console.WriteLine("Greater number: " + n1);
+}
+else
+{
+    Console.WriteLine("Greater number: " + n2);
+}
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/04.SimpleStatements/05.GuessThePassword/Program.cs
@@ -0,0 +1,14 @@
+﻿string password = (Console.ReadLine());
+
+if (password == "s3cr3t!")
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
+
+
+
+
