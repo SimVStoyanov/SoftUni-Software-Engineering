e826118dafe4ef42a280a43c22cdacfc1a793766
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-07 23:31:13 +0300

    Exercise Data Types

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/03.ExerciseDataTypes/04.FoodDelivery/Program.cs
@@ -0,0 +1,20 @@
+﻿double chickenMenu = 10.35;
+double fishMenu = 12.40;
+double vegetarianMenu = 8.15;
+double delivery = 2.50;
+
+double chickenMenuOrderQuantity = double.Parse(Console.ReadLine());
+double fishMenuOrderQuantity = double.Parse(Console.ReadLine());
+double vegetarianMenuOrderQuantity = double.Parse(Console.ReadLine());
+
+double chickenOrderCost = chickenMenuOrderQuantity * chickenMenu;
+double fishOrderCost = fishMenuOrderQuantity * fishMenu;
+double vegeterianOrderCost = vegetarianMenuOrderQuantity * vegetarianMenu;
+
+double totalMenuOrderCost = chickenOrderCost + fishOrderCost + vegeterianOrderCost;
+
+double desertOrderPercentage = 0.2;
+double totalDesertOrderCost = totalMenuOrderCost * desertOrderPercentage;
+
+double totalCost = totalMenuOrderCost + totalDesertOrderCost + delivery;
+Console.WriteLine(totalCost);
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/03.ExerciseDataTypes/05.BasketballEquipment/Program.cs
@@ -0,0 +1,9 @@
+﻿double yearlytTrainingFee = double.Parse(Console.ReadLine());
+
+double sneakersCost = yearlytTrainingFee * 0.6; 
+double uniformCost = sneakersCost * 0.8; 
+double ballCost = uniformCost * 0.25;
+double accessoriesCost = ballCost * 0.2;
+
+double totalCost = yearlytTrainingFee + sneakersCost + uniformCost + ballCost + accessoriesCost;
+Console.WriteLine(totalCost);
