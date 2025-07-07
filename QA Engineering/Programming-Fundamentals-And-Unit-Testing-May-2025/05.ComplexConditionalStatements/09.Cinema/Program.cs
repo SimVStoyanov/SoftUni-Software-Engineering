bbc77257acb6811202c751c20779d90820302cac
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-08 00:02:12 +0300

    Complex Conditional Statements

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/05.ComplexConditionalStatements/09.Cinema/Program.cs
@@ -0,0 +1,23 @@
+﻿string movieType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int seatsPerRow = int.Parse(Console.ReadLine());
+
+double ticketPrice = 0.0;
+
+switch (movieType)
+{
+    case "Premiere":
+        ticketPrice = 12.00;
+        break;
+    case "Normal":
+        ticketPrice = 7.50;
+        break;
+    case "Discount":
+        ticketPrice = 5.00;
+        break;
+}
+
+int totalSeats = rows * seatsPerRow;
+double totalPrice = totalSeats * ticketPrice;
+
+Console.WriteLine($"{totalPrice:F2}");
