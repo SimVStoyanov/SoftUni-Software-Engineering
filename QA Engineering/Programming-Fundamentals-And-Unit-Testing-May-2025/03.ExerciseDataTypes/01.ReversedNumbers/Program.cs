7fbcfd6450b9a07f3cab99b75c92d25d86e7047e
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-09 21:53:11 +0300

    03.ExerciseDataTypes

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/03.ExerciseDataTypes/01.ReversedNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+double tirtNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine(tirtNumber);
+Console.WriteLine(secondNumber);
+Console.WriteLine(firstNumber);
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/03.ExerciseDataTypes/06.Aquarium/Program.cs
@@ -0,0 +1,13 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percetange = double.Parse(Console.ReadLine());
+
+double aquariumVolumeInCm = length * width * height;
+double aquariumVolumeInLiters = aquariumVolumeInCm * 0.001;
+
+double occupiedSpace = percetange * 0.010;
+double result = aquariumVolumeInLiters * (1 - occupiedSpace);
+
+Console.WriteLine($"{result:F2}");
+
