907792a833d5f8c4835aab77814d6afd4bc3fedd
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-09 21:58:38 +0300

    13.Arrays

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/13.Arrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿int day = int.Parse(Console.ReadLine());
+
+string[] daysOfWeek = new string[]
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("Invalid day!");
+}
+else
+{
+    Console.WriteLine(daysOfWeek[day - 1]);
+}
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/13.Arrays/04.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse)
+                       .ToArray();
+
+int evenSum = 0;
+int oddSum = 0;
+
+foreach (int item in numbers)
+{
+    if (item % 2 == 0) 
+    {
+        evenSum += item;
+    }
+    else 
+    {
+        oddSum += item;
+    }
+}
+
+int difference = evenSum - oddSum;
+
+Console.WriteLine(difference);
