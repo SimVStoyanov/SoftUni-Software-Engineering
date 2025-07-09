8c16fdf0ca04744cdd30434c9a240535d8675a1d
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-09 22:04:16 +0300

    19.Unit Testing - Lists

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/19.Unit Testing - Lists/TestApp.UnitTests/IgnoreTheCharsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class IgnoreTheCharsTests
+{
+    [Test]
+    public void Test_IgnoreChars_EmptyStringSentence_ReturnsEmptyString()
+    {
+        //Arrange
+        string sentence = "";
+        List<char> forbiddenChars = new List<char> { 'a', 'e' };
+
+        //Act
+        string resultSentence = IgnoreTheChars.IgnoreChars(sentence, forbiddenChars);
+
+        //Assert
+        Assert.That(sentence, Is.EqualTo(resultSentence));
+    }
+
+    [Test]
+    public void Test_IgnoreChars_EmptyList_ReturnsSameSentence()
+    {
+        //Arrange
+        string sentence = "Hello world";
+        List<char> forbiddenChars = new List<char>();
+
+        //Act
+        string resultSentence = IgnoreTheChars.IgnoreChars(sentence, forbiddenChars);
+
+        //Assert
+        Assert.That(sentence, Is.EqualTo(resultSentence));
+    }
+
+    [Test]
+    public void Test_IgnoreChars_OneCharSentenceAndSameCharsForIgnoring_ReturnsEmptyString()
+    {
+        //Arrange
+        string sentence = "m";
+        List<char> forbiddenChars = new List<char> { 'm' };
+        string expectedSentence = "";
+
+        //Act
+        string resultSentence = IgnoreTheChars.IgnoreChars(sentence, forbiddenChars);
+
+        //Assert
+        Assert.That(expectedSentence, Is.EqualTo(resultSentence));
+    }
+
+    [Test]
+    public void Test_IgnoreChars_WholeSentenceAndFewCharsToIgnore_ReturnsCorrectString()
+    {
+        //Arrange
+        string sentence = "Hello world";
+        List<char> forbiddenChars = new List<char> { 'l', 'o' };
+        string expectedSentence = "He wrd";
+
+        //Act
+        string resultSentence = IgnoreTheChars.IgnoreChars(sentence, forbiddenChars);
+
+        //Assert
+        Assert.That(expectedSentence, Is.EqualTo(resultSentence));
+    }
+}
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/19.Unit Testing - Lists/TestApp.UnitTests/ListAnalyzerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class ListAnalyzerTests
+{
+    [Test]
+    public void Test_Analyze_EmptyList_ShouldReturnNoElementsMsg()
+    {
+        //Arrange
+        List<int> emptyList = new List<int>();
+        string expectedText = "No elements!";
+
+        //Act
+        string resultText = ListAnalyzer.Analyze(emptyList);
+
+        //Assert
+        Assert.That(resultText, Is.EqualTo(expectedText));
+    }
+
+    [Test]
+    public void Test_Analyze_OneElement_ShouldReturnSameValueForMinMaxAvg()
+    {
+        //Arrange
+        List<int> listWithOneElement = new List<int> { 6 };
+        string expectedText = "Element count: 1, Min value: 6, Max value: 6, Avg: 6.00.";
+
+        //Act
+        string resultText = ListAnalyzer.Analyze(listWithOneElement);
+
+        //Assert
+        Assert.That(resultText, Is.EqualTo(expectedText));
+    }
+
+    [Test]
+    public void Test_Analyze_OnlySameValueElements_ShouldReturnSameValueForMinMaxAvg()
+    {
+        //Arrange
+        List<int> listWithSameElements = new List<int> { 6, 6, 6 };
+        string expectedText = "Element count: 3, Min value: 6, Max value: 6, Avg: 6.00.";
+
+        //Act
+        string resultText = ListAnalyzer.Analyze(listWithSameElements);
+
+        //Assert
+        Assert.That(resultText, Is.EqualTo(expectedText));
+    }
+
+    [Test]
+    public void Test_Analyze_DiffrentValues_ShouldReturnCorrectValues()
+    {
+        //Arrange
+        List<int> numbers = new List<int> { 3, 4, 5, 7 };
+        string expectedText = "Element count: 4, Min value: 3, Max value: 7, Avg: 4.75.";
+
+        //Act
+        string resultText = ListAnalyzer.Analyze(numbers);
+
+        //Assert
+        Assert.That(resultText, Is.EqualTo(expectedText));
+    }
+}
