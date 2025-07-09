0ed445754fb1c363c0b5a272d623d77cba79bbdc
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-09 21:55:29 +0300

    08.While Loop

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/08.While Loop/04.Sequence2kPlus1/Program.cs
@@ -0,0 +1,10 @@
+﻿int range = int.Parse(Console.ReadLine());
+
+int number = 1;
+
+while (number <= range)
+{
+    Console.WriteLine(number);
+
+    number = number * 2 + 1;
+}
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/08.While Loop/06.NumberInRange/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+while (number < 1 || number > 100)
+{
+    number = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(number);
+
+while (true)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num >= 1 && num <= 100)
+    {
+        Console.WriteLine(num);
+        break;
+    }
+}
