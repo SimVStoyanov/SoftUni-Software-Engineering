5ef5d4bda1a97bc6289c4de014c47aab9cd023e2
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-09 21:53:01 +0300

    02.KeyConcepts

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/02.KeyConcepts/03.Market/Program.cs
@@ -0,0 +1,13 @@
+﻿double tomatoPrice = double.Parse(Console.ReadLine());
+double tomatoQuantity = double.Parse(Console.ReadLine());
+double cucumberPricee = double.Parse(Console.ReadLine());
+double cucumberQuantity = double.Parse(Console.ReadLine());
+
+double sumTomato = tomatoQuantity * tomatoPrice;
+double sumCucumber = cucumberQuantity * cucumberPricee;
+
+double totalPrice = sumTomato + sumCucumber;
+
+Console.WriteLine($"{totalPrice:F2}");
+
+
---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/02.KeyConcepts/06.MandatoryLiterature/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int numberOfPage = int.Parse(Console.ReadLine());
+int pagesReadInHour = int.Parse(Console.ReadLine());
+int daysToFinish = int.Parse(Console.ReadLine());
+
+int totalReadingTimeOfTheBook = numberOfPage / pagesReadInHour;
+int requiredHoursPerDay = totalReadingTimeOfTheBook / daysToFinish;
+
+Console.WriteLine(requiredHoursPerDay);
