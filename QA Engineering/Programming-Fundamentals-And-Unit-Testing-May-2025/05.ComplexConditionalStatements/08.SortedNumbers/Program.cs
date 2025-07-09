59793ac419dd47e72177888461c86737d2ef520d
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-07-09 21:54:26 +0300

    05.ComplexConditionalStatements

---
+++ QA Engineering/Programming-Fundamentals-And-Unit-Testing-May-2025/05.ComplexConditionalStatements/08.SortedNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+
+if (n1 < n2 && n2 < n3)
+{
+    Console.WriteLine("Ascending");
+}
+else if (n1 > n2 && n2 > n3)
+{
+    Console.WriteLine("Descending");
+}
+else
+{
+    Console.WriteLine("Not sorted");
+}
