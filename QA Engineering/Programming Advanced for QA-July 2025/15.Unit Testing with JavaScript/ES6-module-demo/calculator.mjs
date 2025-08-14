0706ed3d3b0e2e997d6558f8ca26939f1a034ba7
Simeon Stoyanov <106108583+SimVStoyanov@users.noreply.github.com>
2025-08-14 18:07:05 +0300

    Unit Testing with JavaScript

---
+++ QA Engineering/Programming Advanced for QA-July 2025/15.Unit Testing with JavaScript/ES6-module-demo/calculator.mjs
@@ -0,0 +1,28 @@
+let x = 10;
+
+ let y = 20;
+
+ function sum (a, b){
+    return a + b
+ }
+
+ function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide (a, b){
+    return a / b
+}
+
+ export {
+    x,
+    y,
+    sum,
+    subtract,
+    multiply,
+    divide
+ }
